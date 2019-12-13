cb06fe78ed7f2cc0c582e46f2bacc44ac0813741
Daniel Mattox <dmattox10@gmail.com>
2019-12-13 12:29:43 -0500

    MVP

--- starwars/src/components/SWCard.js
+++ starwars/src/components/SWCard.js
@@ -3,32 +3,25 @@ import { Card, Button, CardHeader, CardFooter, CardBody,
     CardTitle, CardText } from 'reactstrap';
 import axios from 'axios'
 
+import FilmText from './FilmText'
+
 const SWCard = (props) => {
 
     const [homeworld, updateHomeworld] = useState('')
-    const [films, updateFilms] = useState([])
 
     useEffect(() => {
         axios.get(props.starPerson.homeworld)
             .then(res => {
                 updateHomeworld(res.data.name)
             })
-        props.starPerson.films.forEach(film => {
-            axios.get(film)
-                .then(res => {
-                    updateFilms(oldFilms => [...oldFilms, res.data.title])
-                })
-        })
-        console.log(films)
     }, [])
-
     return (
         <Card>
             <CardHeader tag="h3">{ props.starPerson.name }</CardHeader>
             <CardBody>
-            <CardTitle>Of { homeworld }</CardTitle>
-                {films.forEach(film => 
-                    <CardText>{ film }</CardText>)}
+            <CardTitle>Of { homeworld } Appears in:</CardTitle>
+                {props.starPerson.films.forEach(film => 
+                    <FilmText film={ film } />)}
             </CardBody>
             <CardFooter className="text-muted">Born: { props.starPerson.birth_year }</CardFooter>
         </Card>
