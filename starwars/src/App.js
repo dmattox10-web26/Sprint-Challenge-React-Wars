7ab80582a3272587c3269a2886bd22b7c76dd157
Daniel Mattox <dmattox10@gmail.com>
2019-12-13 11:37:06 -0500

    API Call good

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { Container, Row, Col } from 'reactstrap'
+
+import Card from './components/Card'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/slate/bootstrap.min.css';
@@ -11,10 +16,38 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+  const [starPeople, updatePeople] = useState([])
+  const [currentPage, updatePage] = useState(1)
 
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people/?page=${currentPage}`)
+      .then(res => {
+        updatePeople(res.data.results)
+      })
+  }, [currentPage])
+
+  const fwdPage = () => {
+
+  }
+
+  const backPage = () => {
+
+  }
+  
+  return (
+    <div className='App'>
+      <h1 className='Header'>React Wars</h1>
+      <Container>
+        {starPeople.map(starPerson => 
+          <Row>
+            <Col xs='3'></Col>
+            <Col x2='6'>
+              <Card starPerson={ starPerson }/>
+            </Col>
+            <Col x2='3'></Col>
+          </Row>
+        )}
+      </Container>
     </div>
   );
 }
---
+++ starwars/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = (props) => {
+    console.log(props.starPerson)
+    return (
+        <div>
+
+        </div>
+    )
+} 
+
+export default Card
